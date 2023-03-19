82f707aefa59e163793f0e30c69335e0eba21745
Pratyush <112469416+Pratyush6978@users.noreply.github.com>
2023-03-19 15:01:44 +0545

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -7,15 +7,11 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown").innerHTML = days + " days";
 
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = "EXAM IS ON";
   }
-}, 1000);
+}, 86400000);
